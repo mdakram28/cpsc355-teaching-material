f039239412963f5c95fa743ca022f7389c873c16
mdakram28 <mohdakram.ansari@ucalgary.ca>
2022-11-03 01:43:57 -0600

    fix

--- session_15/ex3.asm
+++ session_15/ex3.asm
@@ -9,13 +9,13 @@ str_fmt:.string "Color( r=%d, g=%d, b=%d )\n"
         color_g_s = 4
         color_b_s = 8
 
-	
+
 	define(col_base_r, x22)
         main_alloc = -(16 + color_size) & -16
         col_s = 16
 main:	stp	x29, x30, [sp, main_alloc]!
 	mov	x29, sp
-	
+
 	add	col_base_r, x29, col_s
 	mov	w19, 100
 	str	w19, [col_base_r, color_r_s]
@@ -34,7 +34,7 @@ main:	stp	x29, x30, [sp, main_alloc]!
         ldr     w2, [col_base_r, color_g_s]
         ldr     w3, [col_base_r, color_b_s]
         bl      printf
-	
+
         mov	x0, 0
 	ldp	x29, x30, [sp], -main_alloc
 	ret
@@ -43,11 +43,16 @@ main:	stp	x29, x30, [sp, main_alloc]!
 
 
 
+
+
+
+
+
         lighten_alloc = -(16) & -16
-lighten:	stp	x29, x30, [sp, lighten_alloc]!
+lighten:stp	x29, x30, [sp, lighten_alloc]!
 	mov	x29, sp
-	
-        // Compute newcol r,g,b
+
+        // Modify origcol r,g,b
         ldr     w19, [x0, color_r_s]
         mul     w19, w19, w1
         str     w19, [x0, color_r_s]
@@ -57,6 +62,27 @@ lighten:	stp	x29, x30, [sp, lighten_alloc]!
         ldr     w19, [x0, color_b_s]
         mul     w19, w19, w1
         str     w19, [x0, color_b_s]
-        	
+
         ldp	x29, x30, [sp], -lighten_alloc
-	ret 
+	ret
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
